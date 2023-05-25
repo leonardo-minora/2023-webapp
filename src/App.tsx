a8ed2185a904ccb0c8307a0a34c63f13f8d9414b
L A Minora <leonardo.minora@gmail.com>
2023-05-25 10:35:30 -0300

    Adicionado comoponente react de lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./App.css";
 
 // componente REact vazio
@@ -15,30 +15,72 @@ const Botao = (props: any) => {
   // }
   return (
     <>
-      <button onClick={() => contar(contador+1)}>
+      <button onClick={() => contar(contador + 1)}>
         {`${titulo} (${contador})`}
       </button>
     </>
   );
 }
 
+const ListaItem = (props: any) => {
+  return <li>{props.titulo}</li>
+}
+
+const Lista = () => {
+  const tarefas = [
+    "Baixar o projeto do github",
+    "Instalar bibliotecas do projeto",
+    "Executar apliação web",
+    "Programar em js e React",
+    "Guardar as modificações no repositório local",
+    "Publicar as modificações no github"
+  ];
+  return (
+    <div className="card">
+      <ul>
+        {tarefas.map( (item, ind) => <ListaItem key={ind} titulo={item} />)
+        /* 
+        [
+          <ListaItem indice={0} titulo={"Baixar o projeto do github"} />,
+          <ListaItem indice={1} titulo={"Instalar bibliotecas do projeto"} />,
+          <ListaItem indice={2} titulo={"Executar apliação web"} />,
+          <ListaItem indice={3} titulo={"Programar em js e React"} />,
+          <ListaItem indice={4} titulo={"Guardar as modificações no repositório local"} />,
+          <ListaItem indice={5} titulo={"Publicar as modificações no github"} /> 
+        ]       
+       */
+      }
+      </ul>
+      <ul>
+        <ListaItem indice={0} titulo={tarefas[0]} />
+        <ListaItem indice={1} titulo={tarefas[1]} />
+        <ListaItem indice={2} titulo={tarefas[2]} />
+        <ListaItem indice={3} titulo={tarefas[3]} />
+        <ListaItem indice={4} titulo={tarefas[4]} />
+        <ListaItem indice={5} titulo={tarefas[5]} />
+      </ul>
+    </div>
+  );
+}
+
 const App = () => {
   const [contador, contar] = useState(10);
 
-	return (
-		<>
-			<div>
+  return (
+    <>
+      <div>
         <h1>Meu contador esta em {contador}</h1>
-			</div>
-			<div className="card">
-        <Botao titulo="Incrementar"/>
+      </div>
+      <div className="card">
+        <Botao titulo="Incrementar" />
         <Botao titulo={"Decrementar"} />
-        <Botao titulo={"zerar"}/>
+        <Botao titulo={"zerar"} />
         <button onClick={() => contar(contador + 1)}>Estado geral</button>
         <Componente />
-			</div>
-		</>
-	);
+      </div>
+      <Lista />
+    </>
+  );
 }
 
 export default App;
